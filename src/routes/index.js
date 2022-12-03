00efc27d69464ffe2eecc5e8ba1996d787ed918e
mauro-balades <mauro.balades@tutanota.com>
2022-12-03 10:44:03 +0100

    changed index to ejs format

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,5 @@ import path from "path";
 const __dirname = path.resolve();
 
 export default (req, res) => {
-    res.sendFile(__dirname + "/public/pages/index.html");
+    res.render("index.ejs");
 }
